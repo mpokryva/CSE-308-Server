2737a324dad20e1fc224929d1473be226669cd09
mpokryva <mpokryva@gmail.com>
2018-11-12 22:51:46 -0500

    Add initial Spring files.

---
+++ src/main/java/com/broncos/gerrymandering/gerrymandering/GerrymanderingApplication.java
@@ -0,0 +1,12 @@
+package com.broncos.gerrymandering.gerrymandering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerrymanderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerrymanderingApplication.class, args);
+	}
+}
