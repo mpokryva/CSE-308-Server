229748598ebf589c442d6b333d57142d33842a90
mpokryva <mpokryva@gmail.com>
2018-11-13 21:37:45 -0500

    Add district-info, district-geo, state-info, and state-geo endpoints.

--- src/main/java/com/broncos/gerrymandering/gerrymandering/Controller.java
+++ src/main/java/com/broncos/gerrymandering/gerrymandering/Controller.java
@@ -1,7 +1,7 @@
 package com.broncos.gerrymandering.gerrymandering;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,7 +27,7 @@ public class Controller {
 
     @RequestMapping(value = "/precinct-info",
             method = RequestMethod.GET,
-            produces = "application/json")
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public String getPrecinctInfo(@RequestParam(value = "geoid") String geoid) throws Exception {
         final String pathname = "data/ny_precs_info.json";
         JSONObject precinctsInfo = getJsonFromFile(pathname);
@@ -36,17 +36,16 @@ public class Controller {
 
     @RequestMapping(value = "/precinct-geo",
             method = RequestMethod.GET,
-            produces = "application/json")
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public String getPrecinctBoundaries(@RequestParam(value = "districtid") String districtid) throws Exception {
         final String pathname = "data/ny_precs_geo.json";
         JSONObject precinctsBoundaries = getJsonFromFile(pathname);
         JSONObject response = new JSONObject();
         Iterator<String> it = precinctsBoundaries.keys();
-        while(it.hasNext()) {
+        while (it.hasNext()) {
             String key = it.next();
             JSONObject precinctBoundaries = precinctsBoundaries.getJSONObject(key);
             JSONObject properties = precinctBoundaries.getJSONObject(PROPERTIES_KEY);
-            System.out.println(properties);
             if (properties != null && properties.getString(DISTRICT_ID_KEY).equals(districtid)) {
                 response.append(key, precinctBoundaries);
             }
@@ -54,9 +53,43 @@ public class Controller {
         return response.toString();
     }
 
+    @RequestMapping(value = "/district-info",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getDistrictInfo(@RequestParam(value = "districtid") String districtid) throws Exception {
+        final String pathname = "data/ny_dist_info.json";
+        JSONObject districtsInfo = getJsonFromFile(pathname);
+        return districtsInfo.getJSONObject(districtid).toString();
+    }
+
+    @RequestMapping(value = "/district-geo",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getDistrictBoundaries(@RequestParam(value = "statecode") String statecode) throws Exception {
+        final String pathname = "data/ny_dist_geo.json";
+        JSONObject districtsBoundaries = getJsonFromFile(pathname);
+        return districtsBoundaries.toString();
+    }
+
+    @RequestMapping(value = "/state-info",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getStateInfo(@RequestParam(value = "statecode") String statecode) throws Exception {
+        final String pathname = "data/ny_info.json";
+        JSONObject stateInfo = getJsonFromFile(pathname);
+        return stateInfo.toString();
+    }
+
+    @RequestMapping(value = "/state-geo",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getStateBoundaries(@RequestParam(value = "statecode") String statecode) throws Exception {
+        final String pathname = "data/ny_geo.json";
+        JSONObject stateBoundaries = getJsonFromFile(pathname);
+        return stateBoundaries.toString();
+    }
 
     private JSONObject getJsonFromFile(String pathname) {
-        ObjectMapper mapper = new ObjectMapper();
         try {
             String content = new String(Files.readAllBytes(Paths.get(pathname)));
             return new JSONObject(content);
